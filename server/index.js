cb8b80824b1a9ad7c0c3c5e9b7ef0f3a4aebcce8
Sixth <sixth@example.com>
2026-05-25 00:28:27 -0300

    remove sqlite for vercel deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import Database from 'better-sqlite3';
+//import Database from 'better-sqlite3';
 import dotenv from 'dotenv';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
